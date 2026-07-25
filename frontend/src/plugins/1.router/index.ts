1b03ba9ce519f9818849f09fed9cfac0cc918b5e
Developer <dev@ngernngern.local>
2026-07-25 09:38:44 +0000

    fix: generate route types before typecheck via vite build

--- frontend/src/plugins/1.router/index.ts
+++ frontend/src/plugins/1.router/index.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue'
 
 import { setupLayouts } from 'virtual:generated-layouts'
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router/auto'
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 function recursiveLayouts(route: RouteRecordRaw): RouteRecordRaw {
   if (route.children) {
